460aa05069bbc7f815f7e8c6268416815abe6247
stevan23k <esteproo@gmail.com>
2026-05-27 13:12:27 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'http://32.197.219.117',
+      'https://32.197.219.117',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
